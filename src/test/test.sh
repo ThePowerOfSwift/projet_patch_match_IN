eb2f56540b6c3d22932ae53177e6b4308435da1e
ocey <g6k81@hotmail.fr>
2015-05-27 16:01:53 +0200

    yeaaaaah rotatiooooon

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,4 +1,4 @@
 #g++ -lopencv_core -lopencv_imgproc -lopencv_highgui pm_minimal.cpp pm_minimal.h distance_rotation.cpp distance_rotation.h brent.cpp brent.h -o test
 
-g++  -lopencv_core -lopencv_imgproc -lopencv_highgui distance_rotation.cpp distance_rotation.h brent.cpp brent.h MSE.cpp MSE.h test.cpp -o test $(pkg-config opencv --cflags --libs)
+g++  -lopencv_core -lopencv_imgproc -lopencv_highgui ../Projet/distance_rotation.cpp ../Projet/distance_rotation.h ../Projet/brent.cpp ../Projet/brent.h ../Projet/BITMAP.cpp ../Projet/BITMAP.h ../Projet/MSE.cpp ../Projet/MSE.h test.cpp -o test $(pkg-config opencv --cflags --libs)
 
